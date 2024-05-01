23a32c4a9052627cca48c96fb28cf6d45b7fef86
plmandreza <plmandreza@gmail.com>
2024-05-01 18:16:27 -0300

    aulas operadores

---
+++ tipos-variaveis/src/OperadoresAritmeticos.java
@@ -0,0 +1,28 @@
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        //String nomeCompleto = "LINGUAGEM" + "JAVA"; 
+        //System.out.println(nomeCompleto);
+
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+    }
+}
---
+++ tipos-variaveis/src/OperadoresRelacionais.java
@@ -0,0 +1,31 @@
+public class OperadoresRelacionais {
+
+    public static void main(String[] args) {
+        String nomeUm = "NOME";
+        String nomeDois = new String("NOME");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+       /* int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+        
+        System.out.println(simNao);
+
+        if(numero1 > numero2)
+	        System.out.print("Numero 1 maior que numero 2");
+
+        if(numero1 < numero2)
+	        System.out.print("Numero 1 menor que numero 2");
+
+        if(numero1 >= numero2)
+	    System.out.print("Numero 1 maior ou igual que numero 2");
+
+        if(numero1 <= numero2)
+	        System.out.print("Numero 1 menor ou igual que numero 2");
+
+        if(numero1 != numero2)
+	        System.out.print("Numero 1 é diferente de numero 2"); */
+        }
+}
