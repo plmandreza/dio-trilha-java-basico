50ca3143c2ad555d772a7e5ad0893eed9589064f
plmandreza <plmandreza@gmail.com>
2024-05-03 22:33:23 -0300

    aulas escopo

---
+++ tipos-variaveis/src/Conta.java
@@ -0,0 +1,32 @@
+public class Conta {
+	//variavel da classe conta 
+    //variável de escopo de classe
+	double saldo=10.0; //double é o tipo da variável
+    //saldo é a úniqca variável que pode ser usada em outros momentos
+	
+	public void sacar(Double valor) { //método 'sacar' tem como parâmetro 'valor'
+		//variavel local de método
+		double novoSaldo = saldo - valor;
+	}
+	public void imprimirSaldo(){
+		//disponível em toda classe
+		System.out.println(saldo);
+		//somente o método sacar conhece esta variavel
+		System.out.println(novoSaldo);
+	
+	}
+	public double calcularDividaExponencial(){
+		//variável local de método
+		double valorParcela = 50.0;
+		double valorMontante = 0.0; // começando a variável com valor zero
+		for(int x=1; x<=5; x++) {//x variável de escopo de fluxo
+			//esta variável será reiniciada a cada execução for
+			double valorCalculado = valorParcela * x;
+			valorMontante = valorMontante + valorCalculado;
+		}
+		//escopo de fluxo
+		//x e valorCalculado nunca estarão disponíveis fora do for
+		
+		return valorMontante; 
+	}
+}
