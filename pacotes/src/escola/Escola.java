4874cd91a20b75a267c0b8c8bab4d4d0b8de056c
plmandreza <plmandreza@gmail.com>
2024-05-16 21:08:19 -0300

    aula colocando getters e setters manualmente

--- pacotes/src/escola/Escola.java
+++ pacotes/src/escola/Escola.java
@@ -3,9 +3,9 @@ package escola;
 public class Escola {
 	public static void main(String[] args) {
 		Aluno felipe = new Aluno();
-		felipe.nome = "Felipe";
-		felipe.idade = 8;
+		felipe.setNome("Felipe Junior");
+		felipe.setIdade(12);
 		
-		System.out.println("O aluno " + felipe.nome + " tem " + felipe.idade + " anos ");
+		System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos ");
 	}
 }
