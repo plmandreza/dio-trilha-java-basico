489de5cc5ffd0d7734d90d64e5aa9173e69052a1
plmandreza <plmandreza@gmail.com>
2024-05-16 21:46:39 -0300

    aula construtores exemplo pessoa

--- pacotes/src/escola/Aluno.java
+++ pacotes/src/escola/Aluno.java
@@ -3,17 +3,17 @@ package escola;
 public class Aluno {
 	private String nome;
 	private int idade;
-	
+
 	public String getNome() {
 		return nome;
 	}
-	public void setNome(String newNome) {
-		nome = newNome;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 	public int getIdade() {
 		return idade;
 	}
-	public void setIdade(int newIdade) {
-		idade = newIdade;
+	public void setIdade(int idade) {
+		this.idade = idade;
 	}
 }
