a1f3dced46ec65e1ff3728e07179b7fd44816d57
plmandreza <plmandreza@gmail.com>
2024-05-20 16:53:38 -0300

    aula pilares poo autódromo polimorfismo e abstração

--- pacotes/src/pilarespoo/Autodromo.java
+++ pacotes/src/pilarespoo/Autodromo.java
@@ -4,10 +4,16 @@ public class Autodromo {
 	public static void main(String[] args) {
 		Carro jeep = new Carro();
 		jeep.setChassi("897897"); 
-		jeep.ligar();
+		//jeep.ligar();
 		
 		Moto z400 = new Moto();
 		z400.setChassi("467754"); 
-		z400.ligar(); //abstração
+		//z400.ligar(); //abstração
+		
+		//Veiculo coringa = jeep; 
+		Veiculo coringa = z400; //polimorfismo e abstração
+		
+		coringa.ligar();
+		//"mantém  o método, porém não sabe qual será o comportamento"
 	}
 }
