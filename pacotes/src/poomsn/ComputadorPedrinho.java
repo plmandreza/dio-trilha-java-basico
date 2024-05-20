4b7a825dbca5d44e78d9cf726cd0e3102d210eef
plmandreza <plmandreza@gmail.com>
2024-05-20 20:07:56 -0300

    aula polimorfismo messengers

--- pacotes/src/poomsn/ComputadorPedrinho.java
+++ pacotes/src/poomsn/ComputadorPedrinho.java
@@ -2,19 +2,24 @@ package poomsn;
 
 public class ComputadorPedrinho {
 	public static void main(String[] args) {
-		System.out.println("MSN");
-		MSNMessenger msn = new MSNMessenger();
-		msn.enviarMensagem();	
-		msn.receberMensagem();
 		
-		System.out.println("FACEBOOK");
-		FacebookMessenger fcb = new FacebookMessenger();
-		fcb.enviarMensagem();
-		fcb.receberMensagem();
+		ServicoMensagemInstantanea smi = null;
 		
-		System.out.println("TELEGRAM");
-		Telegram tlg = new Telegram();
-		tlg.enviarMensagem();
-		tlg.receberMensagem();
+		/*
+		    NÃO SE SABE QUAL APP 
+		    MAS QUALQUER UM DEVERÁ ENVIAR E RECEBER MENSAGEM
+		 */
+		
+		String appEscolhido="tlg"; //polimorfismo
+		
+		if(appEscolhido.equals("msn"))
+			smi = new MSNMessenger();
+		else if(appEscolhido.equals("fbm"))
+			smi = new FacebookMessenger();
+		else if(appEscolhido.equals("tlg"))
+			smi = new Telegram();
+			
+		smi.enviarMensagem();
+		smi.receberMensagem();
 	}
 }
