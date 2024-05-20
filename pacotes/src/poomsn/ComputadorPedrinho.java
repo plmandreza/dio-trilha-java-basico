8de32688aa0217bf7de677a3b7aae735d9e8abab
plmandreza <plmandreza@gmail.com>
2024-05-20 18:54:48 -0300

    aulas pilares poo msn messenger sem encapsulamento

---
+++ pacotes/src/poomsn/ComputadorPedrinho.java
@@ -0,0 +1,17 @@
+package poomsn;
+
+/*
+ * Simulação do uso da classe MSNMessenger
+ */
+public class ComputadorPedrinho {
+	public static void main(String[] args) {
+		//abrindo MSN Messenger
+		MSNMessenger msn = new MSNMessenger();
+		
+		msn.validarConectadoInternet();
+		msn.enviarMensagem();
+		msn.salvarHistoricoMensagem();
+		
+		msn.receberMensagem();
+	}
+}
