2329473b1cbef0a0d20db849f86bb0bd763f846b
plmandreza <plmandreza@gmail.com>
2024-05-26 20:38:49 -0300

    aulas pesquisa List

---
+++ collections-java-api-2023/src/main/java/list/pesquisa/Livro.java
@@ -0,0 +1,38 @@
+package main.java.list.pesquisa;
+
+public class Livro {
+    //atributos
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+    //construtor
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    //getter para atributos private
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    //formata para não exibir o endereço de memória
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
+    }
+}
