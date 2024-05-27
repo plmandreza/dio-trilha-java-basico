3b260c65b429d8fc01a01db300c08c0e3a5d4d12
plmandreza <plmandreza@gmail.com>
2024-05-26 23:46:01 -0300

    aula Collections Set pesquisa

---
+++ collections-java-api-2023/src/main/java/set/pesquisa/AgendaContatos.java
@@ -0,0 +1,77 @@
+package main.java.set.pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    //atributo
+    private Set<Contato> contatosSet;
+
+    //construtor
+    public AgendaContatos() {
+        this.contatosSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero) {
+        contatosSet.add(new Contato(nome, numero));
+    }
+
+    public void exibirContatos() {
+        System.out.println(contatosSet);
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome) {
+        Set<Contato> contatosPorNome = new HashSet<>();
+
+            for (Contato c : contatosSet) {
+                if (c.getNome().startsWith(nome)) {
+                    contatosPorNome.add(c);
+                    break;
+                }
+            }
+        return contatosPorNome;
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero) {
+        Contato contatoAtualizado = null;
+        for (Contato c : contatosSet) {
+            if (c.getNome().equalsIgnoreCase(nome)) {
+                c.setNumero(novoNumero);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+        return contatoAtualizado;
+    }
+
+    public static void main(String[] args) {
+        // Criando uma instância da classe AgendaContatos
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        // Exibindo os contatos no conjunto (deve estar vazio)
+        agendaContatos.exibirContatos();
+
+        // Adicionando contatos à agenda
+        agendaContatos.adicionarContato("João", 123456789);
+        agendaContatos.adicionarContato("Maria", 987654321);
+        agendaContatos.adicionarContato("Maria Fernandes", 55555555);
+        agendaContatos.adicionarContato("Ana", 88889999);
+        agendaContatos.adicionarContato("Fernando", 77778888);
+        agendaContatos.adicionarContato("Carolina", 55555555);
+
+        // Exibindo os contatos na agenda
+        agendaContatos.exibirContatos();
+
+        // Pesquisando contatos pelo nome
+        System.out.println(agendaContatos.pesquisarPorNome("Maria"));
+
+        // Atualizando o número de um contato
+        Contato contatoAtualizado = agendaContatos.atualizarNumeroContato("Carolina", 44443333);
+        System.out.println("Contato atualizado: " + contatoAtualizado);
+
+        // Exibindo os contatos atualizados na agenda
+        System.out.println("Contatos na agenda após atualização:");
+        agendaContatos.exibirContatos();
+    }
+
+}
---
+++ collections-java-api-2023/src/main/java/set/pesquisa/Tarefa.java
@@ -0,0 +1,44 @@
+package main.java.set.pesquisa;
+
+import java.util.Objects;
+
+public class Tarefa {
+    private String descricao;
+    private boolean concluida;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+        this.concluida = false;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tarefa tarefa = (Tarefa) o;
+        return Objects.equals(descricao, tarefa.descricao);
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa{" +
+                "descricao='" + descricao + '\'' +
+                '}';
+    }
+}
